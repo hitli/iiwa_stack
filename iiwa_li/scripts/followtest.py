cb5d298c2979d5d41b71026166ec9f612b46aec4
lizq <124739168@qq.com>
2018-04-24 19:08:12 +0800

    跟随成功
    之前姿态覆盖太晚了。应该在基坐标系下设置姿态

--- iiwa_li/scripts/followtest.py
+++ iiwa_li/scripts/followtest.py
@@ -13,24 +13,34 @@ def follow():
     pub = rospy.Publisher('/iiwa/command/CartesianPose', PoseStamped, queue_size=10)
     rate = rospy.Rate(3)
     TJM = np.loadtxt('/home/lizq/win7share/TJM.txt', delimiter=",")  # mm
+    # TNB = np.loadtxt('/home/lizq/win7share/TNB.txt', delimiter=",")  # mm
+    # TNO = TNB.dot(TBO)
+    TBO = np.loadtxt('/home/lizq/win7share/TBO.txt', delimiter=",")
     TON = np.loadtxt('/home/lizq/win7share/TON.txt', delimiter=",")  # mm
     TNO = inv(TON)
     TCP = np.loadtxt('/home/lizq/win7share/TCP.txt', delimiter=",").tolist()  # m
     TNN = np.array([[1.0,0.0,0.0,-13.5318],[0.0,1.0,0.0,0.50804],[0.0,0.0,1.0,-153.66507],[0.0,0.0,0.0,1.0]])
     while not rospy.is_shutdown():
         try:
-            TMN = qc.quat2matrix(np.loadtxt('/home/lizq/win7share/NDI.txt', delimiter=",", skiprows=1).tolist())
-            TMN = TMN.dot(TNN)#更正钢针位姿
-            TJN = TJM.dot(TMN)
-            quat = list(qc.matrix2quat(TJN))
-            quat[3:] = TCP[3:]
-            TJN = qc.quat2matrix(quat)
-            TJO=TJN.dot(TNO)
+            ndi = np.loadtxt('/home/lizq/win7share/NDI.txt', delimiter=",")
+            # TMN = qc.quat2matrix(np.loadtxt('/home/lizq/win7share/NDI.txt', delimiter=",", skiprows=1).tolist())
+            TMB = qc.quat2matrix(ndi[1].tolist)
+            print 'TMB',ndi[1]
+            # TMB = qc.quat2matrix(np.loadtxt('/home/lizq/win7share/NDI.txt', delimiter=",").tolist())
+            print 'TMN',ndi[2]
+            TMN = qc.quat2matrix(ndi[2].tolist)
+            TMN = TMN.dot(TNN)#更正钢针位置
+            print 'TMN',qc.matrix2quat(TMN)
+            print '钢针TMO',qc.matrix2quat(TMN.dot(TNO))
+            print '刚体TMO',qc.matrix2quat(TMB.dot(TBO))
+            TJO=TJM.dot(TMN).dot(TNO)
             #mm->m
             TJO[0][3] /= 1000
             TJO[1][3] /= 1000
             TJO[2][3] /= 1000
-            command_point = qc.get_command_pose(qc.matrix2quat(TJO))
+            quat =list(qc.matrix2quat(TJO))
+            quat[3:] = TCP[3:]
+            command_point = qc.get_command_pose(quat)
             rospy.loginfo(command_point)
             pub.publish(command_point)
             rate.sleep()

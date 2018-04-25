68ce800b90e642a66bf443c4515b1cf19be7d7e5
lizq <124739168@qq.com>
2018-04-25 18:40:50 +0800

    改为单点偏差计算，去除漂移

--- iiwa_li/scripts/calibrate_error.py
+++ iiwa_li/scripts/calibrate_error.py
@@ -4,31 +4,27 @@
 import rospy
 import numpy as np
 import quaternion_calculate as qc
-from numpy.linalg import inv
 from geometry_msgs.msg import PoseStamped
 
 
+#发送某视觉空间位置点,自动解算TJO,用error.m计算误差
 def follow():
     rospy.init_node('follow', anonymous=True)
     rospy.sleep(1)
-    rate = rospy.Rate(10)
     pub = rospy.Publisher('/iiwa/command/CartesianPose', PoseStamped, queue_size=10)
     TJM = np.loadtxt('/home/lizq/win7share/TJM.txt', delimiter=",")  # mm
     TBO = np.loadtxt('/home/lizq/win7share/TBO.txt', delimiter=",")  # mm
-    while not rospy.is_shutdown():
-        TMB = qc.quat2matrix((-52.2300,64.4000,-1181.0500,-0.4117,-0.1769,-0.8114,0.3749))
-        #-52.3600, 64.4300, -1181.1899, -0.4115, -0.1773, -0.8114, 0.3749
-        # TMB = qc.quat2matrix(np.loadtxt('/home/lizq/win7share/NDI.txt', delimiter=","))
-        TJO=TJM.dot(TMB).dot(TBO)
-        #mm->m
-        TJO[0][3] /= 1000
-        TJO[1][3] /= 1000
-        TJO[2][3] /= 1000
-        quat = list(qc.matrix2quat(TJO))
-        command_point = qc.get_command_pose(quat)
-        rospy.loginfo(command_point)
-        pub.publish(command_point)
-        rate.sleep()
+    TMB = qc.quat2matrix((-52.2300,64.4000,-1181.0500,-0.4117,-0.1769,-0.8114,0.3749))
+    #-52.3600, 64.4300, -1181.1899, -0.4115, -0.1773, -0.8114, 0.3749
+    TJO=TJM.dot(TMB).dot(TBO)
+    #mm->m
+    TJO[0][3] /= 1000
+    TJO[1][3] /= 1000
+    TJO[2][3] /= 1000
+    quat = list(qc.matrix2quat(TJO))
+    command_point = qc.get_command_pose(quat)
+    rospy.loginfo(command_point)
+    pub.publish(command_point)
 
 
 if __name__ == '__main__':
